6ad167547746d8b54817ca3988349dd875550877
jared keown <jkeown@pop-os.localdomain>
2021-09-07 18:44:32 -0400

    added skills section

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,9 +1,14 @@
+<script>
+  import Skills from '$lib/skills.svelte'
+</script>
+
 <section>
   <div>
     <h1 class="bg-gradient"><span>Email</span> Developer</h1>
     <h2>Jared Keown</h2>
   </div>
   <a class="button" href="/projects">My Work</a>
+  <Skills />
 </section>
 
 <style>
