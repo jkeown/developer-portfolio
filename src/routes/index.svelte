bee7d1e7e6dfd8cf3405e5fe45ec6e9e60d58719
jared keown <jkeown@pop-os.localdomain>
2021-09-26 18:56:23 -0400

    title typing effect

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -3,8 +3,8 @@
 </script>
 
 <section>
-  <div>
-    <h1 class="bg-gradient">Web<sup>^<span>and Email</span></sup>Developer</h1>
+  <div class="title">
+    <h1 class="bg-gradient"><span class="typing"></span><span class="cursor">|</span>Developer</h1>
     <h2>Jared Keown</h2>
   </div>
   <a class="button" href="/projects">My Work</a>
@@ -24,20 +24,13 @@
     background-repeat: no-repeat;    
     background-size: cover;
   }
+  .title{
+    min-width: 300px;
+    /* min-width: 83vw; */
+    /* min-width: 370px; */
+  }
   h1{
     font-size: 2rem;
-  }
-  h1 sup{
-    color: var(--dark-gray);
-    font-size: 1.25rem;
-
-  }
-  h1 span{
-    position: absolute;
-    font-family: cursive;
-    font-size: 1.5rem;
-    transform-origin: left;
-    transform: rotate(-15deg) translate(-10px, -20px);
   }
   .button{
     padding: 10px 25px;
@@ -46,11 +39,99 @@
     h1{
     font-size: 2.5rem;
   }
+  .title{
+    min-width: 370px;
+  }
   }
   @media (min-width: 700px){
     section{
     background-size: contain;
   }
   }
-  
+  .cursor{
+    animation: blinking 1s linear infinite;
+    color: var(--dark-gray);
+  }
+  .typing:before {
+  content: "";
+  animation: type-words 7.5s linear infinite;
+  animation-fill-mode: forwards;
+  background-image: var(--green-blue-gradient);
+  color: transparent;
+  background-clip: text;
+  -webkit-background-clip: text;
+  letter-spacing: 2px;
+}
+
+@keyframes blinking{
+  0%{
+    opacity: 1;
+  }
+  50%{
+    opacity: 0;
+  }
+  100%{
+    opacity: 1;
+  }
+}
+
+@keyframes type-words {
+  0% {
+    content: "";
+  }
+  5% {
+    content: "W";
+  }
+  10% {
+    content: "We";
+  }
+  15% {
+    content: "Web";
+  }
+  25% {
+    content: "Web";
+  }
+  30% {
+    content: "We";
+  }
+  35%{
+    content: "W";
+  }
+  40% {
+    content: "";
+  }
+  45% {
+    content: " E";
+  }
+  50% {
+    content: " Em";
+  }
+  55% {
+    content: " Ema";
+  }
+  60% {
+    content: " Emai";
+  }
+  65% {
+    content: "Email";
+  }
+  75% {
+    content: "Email";
+  }
+  80% {
+    content: " Emai";
+  }
+  85% {
+    content: " Ema";
+  }
+  90% {
+    content: " Em";
+  }
+  95% {
+    content: " E";
+  }
+  100% {
+    content: "";
+  }
+}
 </style>
