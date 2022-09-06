29494db8f7e5cf8e637a611630f27b507e3204c7
Jared Keown <jkeown@pop-os.localdomain>
2022-09-06 10:31:28 -0400

    bio text

--- src/lib/about.svelte
+++ src/lib/about.svelte
@@ -6,7 +6,7 @@
   <p>
   Discovering my passion for code a little later in life, I soon found myself sacrificing many things, (including sleep), to start this new adventure.  I slowly transitioned
   from pharmacy technician to freelance web developer utilizing primarily Jamstack technologies.
-  Recently, I've been part of a team responsible for over 400 websites, resulting in my bug hunting and CSS skills having greatly improved, while also learning jQuery.</p><br /><p>I strive for clean code that is easy to read and maintain.  I've been described as pleasant to work with. I'm looking forward to joining a team that promotes growth and takes pride in delivering quality work. When not using my free time to code, you can find me surfing (waves not the internet). 🏄</p>
+  Recently, I was part of a team responsible for over 400 websites, mostly direct-booking vacation rental sites. My experience there greatly improved my bug hunting and CSS skills, while also learning jQuery.</p><br /><p>I strive for clean code that is easy to read and maintain.  I've been described as pleasant to work with. I'm looking forward to joining a team that promotes growth and takes pride in delivering quality work. When not using my free time to code, you can find me surfing (waves not the internet). 🏄</p>
 </section>
 
 <style>
